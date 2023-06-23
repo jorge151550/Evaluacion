a845d90df93485a9c3689c2a8a30d65f4b88df86
jorge151550 <117932316+jorge151550@users.noreply.github.com>
2023-06-23 11:15:17 -0500

    Delete ApiJorge directory

--- ApiJorge/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config(); //Cargar toda la importación
-
-const Server = require('./models/server')
-
-const server = new Server() //Creando un objeto de Server
-
-server.listen()
-
